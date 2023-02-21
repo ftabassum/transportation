83d67d07a675ff306658b8987b505aa96eb76c74
ftabassum <30696758+ftabassum@users.noreply.github.com>
2023-02-21 02:45:45 -0500

    pushing these two files first

--- distancecalc.py
+++ distancecalc.py
@@ -10,13 +10,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import cos, sin, asin, sqrt, radians
 
-# load GTFS MTA Bus Stop file which has stop coordinates (but not according to bus lines) 
-MTA_Bus_Stops = pd.read_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\stops.txt')
+## load GTFS MTA Bus Stop file which has stop coordinates (but not according to bus lines) 
+#MTA_Bus_Stops = pd.read_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\stops.txt')
+MTA_Bus_Stops = pd.read_csv('stops.txt')
 MTA_Bus_Stops = MTA_Bus_Stops.drop(["stop_desc", "zone_id", "stop_url", "location_type", "parent_station"], axis=1)
 
-# load calculated time difference csv file
-df_time_M101 = pd.read_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\Time Difference\M101_timedifference.csv')
+## load calculated time difference csv file
+#df_time_M101 = pd.read_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\Time Difference\M101_timedifference.csv')
+df_time_M101 = pd.read_csv('M101_timedifference.csv')
+
 # merge time and location dataframes to correspond the time difference to a stop location
 M101 = MTA_Bus_Stops.merge(df_time_M101, on = 'stop_id')
 M101["distanceinmiles"] = 3960*2 * np.arcsin(np.sqrt(np.sin((np.radians(M101['stop_lat']) - np.radians(M101.stop_lat.shift(1)))/2)**2 + np.cos(np.radians(M101.stop_lon.shift(1))) * np.cos(np.radians(M101['stop_lon'])) * np.sin((np.radians(M101['stop_lon']) - np.radians(M101.stop_lon.shift(1)))/2)**2))
-M101.to_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\Distances\M101 distances.csv')
+
+#M101.to_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\Distances\M101 distances.csv')
+M101.to_csv('distances.csv')
--- timedifference.py
+++ timedifference.py
@@ -20,19 +20,27 @@ df_shapes = pd.read_csv('shapes.txt')
 df_ts = df_trips.merge(df_shapes, on = "shape_id")
 
 #%%this code loads GTFS MTA Manhattan bus stop times and divides them according to the bus line
-df_time = pd.read_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\stop_times.txt', sep=',' , header=1, names = ["trip_id", "arrival_time", "departure_time", "stop_id", "stop_sequence", "pickup_type", "drop_off_type", "timepoint"])
+#df_time = pd.read_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\stop_times.txt', sep=',' , header=1, names = ["trip_id", "arrival_time", "departure_time", "stop_id", "stop_sequence", "pickup_type", "drop_off_type", "timepoint"])
+df_time = pd.read_csv('stop_times.txt', sep=',' , header=1, names = ["trip_id", "arrival_time", "departure_time", "stop_id", "stop_sequence", "pickup_type", "drop_off_type", "timepoint"])
+
+
 #adding a new column 'Bus_Line' to only include the bus line name
 df_time['Bus_Line'] = df_time["trip_id"].str.split("_").map(lambda x: x[2])
+
 #masking a dataframe
 df_time_M101 = df_time[["arrival_time", "stop_id", "Bus_Line"]].loc[df_time['Bus_Line']=="M101"]
 df_time_M101.drop_duplicates(subset=["stop_id"], keep="first", inplace=True)
 df_time_M101.head()
+
 # there are a lot of suplicate values in the set, why?
 df_time_M101.drop_duplicates(subset=["stop_id"], keep="first", inplace=True)
 df_time_M101.head()
+
 # this code converts time difference into hours (want speed in mph)
 df_time_M101["timetravelledinhours"] = pd.to_timedelta(df_time_M101["arrival_time"]).dt.total_seconds()/3600
 df_time_M101["timedifference"] = df_time_M101["timetravelledinhours"].diff().abs()
+
 # look at where there are negative times in the dataset and why that is the case
 df_time_M101 = df_time_M101.drop(["timetravelledinhours"], axis=1)
-df_time_M101.to_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\Time Difference\M101_timedifference.csv', index=False)
+#df_time_M101.to_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\Time Difference\M101_timedifference.csv', index=False)
+df_time_M101.to_csv('M101_timedifference.csv', index=False)
