c664d32f0b27f3124e33da80136631c87d34dc9c
minah7alil <minah.ali@cooper.edu>
2023-02-22 22:18:38 -0500

    February 22nd

--- distancecalc.py
+++ distancecalc.py
@@ -6,17 +6,25 @@ Created on Wed Feb  8 13:34:22 2023
 """
 
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.pyplot as pltimport numpy as np
 from math import cos, sin, asin, sqrt, radians
 
+#%% This code calculates distances between bus stops for the M101 bus line. These are the shortest distances between coordinates that do not take into account road geometry.
 # load GTFS MTA Bus Stop file which has stop coordinates (but not according to bus lines) 
-MTA_Bus_Stops = pd.read_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\stops.txt')
+MTA_Bus_Stops = pd.read_csv('stops.txt')
 MTA_Bus_Stops = MTA_Bus_Stops.drop(["stop_desc", "zone_id", "stop_url", "location_type", "parent_station"], axis=1)
 
-# load calculated time difference csv file
-df_time_M101 = pd.read_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\Time Difference\M101_timedifference.csv')
+# load previously calculated M101 northbound and southbound csv files with time difference
+df_ts_M101_n = pd.read_csv("M101_northbound.csv")
+df_ts_M101_s = pd.read_csv("M101_southbound.csv")
 # merge time and location dataframes to correspond the time difference to a stop location
-M101 = MTA_Bus_Stops.merge(df_time_M101, on = 'stop_id')
-M101["distanceinmiles"] = 3960*2 * np.arcsin(np.sqrt(np.sin((np.radians(M101['stop_lat']) - np.radians(M101.stop_lat.shift(1)))/2)**2 + np.cos(np.radians(M101.stop_lon.shift(1))) * np.cos(np.radians(M101['stop_lon'])) * np.sin((np.radians(M101['stop_lon']) - np.radians(M101.stop_lon.shift(1)))/2)**2))
-M101.to_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\Distances\M101 distances.csv')
+df_ts_M101_n = df_ts_M101_n.merge(MTA_Bus_Stops, on = 'stop_id')
+df_ts_M101_n["distanceinmiles"] = 3960*2 * np.arcsin(np.sqrt(np.sin((np.radians(df_ts_M101_n['stop_lat']) - np.radians(df_ts_M101_n.stop_lat.shift(1)))/2)**2 + np.cos(np.radians(df_ts_M101_n.stop_lon.shift(1))) * np.cos(np.radians(df_ts_M101_n['stop_lon'])) * np.sin((np.radians(df_ts_M101_n['stop_lon']) - np.radians(df_ts_M101_n.stop_lon.shift(1)))/2)**2))
+df_ts_M101_s = df_ts_M101_s.merge(MTA_Bus_Stops, on = 'stop_id')
+df_ts_M101_s["distanceinmiles"] = 3960*2 * np.arcsin(np.sqrt(np.sin((np.radians(df_ts_M101_s['stop_lat']) - np.radians(df_ts_M101_s.stop_lat.shift(1)))/2)**2 + np.cos(np.radians(df_ts_M101_s.stop_lon.shift(1))) * np.cos(np.radians(df_ts_M101_s['stop_lon'])) * np.sin((np.radians(df_ts_M101_s['stop_lon']) - np.radians(df_ts_M101_s.stop_lon.shift(1)))/2)**2))
+
+#%% Save each dataframe as a csv file
+
+df_ts_M101_n.to_csv("M101_northbound_dist.csv", index=False)
+df_ts_M101_s.to_csv("M101_southbound_dist.csv", index=False)
+
