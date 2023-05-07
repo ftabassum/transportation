15af2a9924e7f384f94b688a0257643ea67b83d3
ftabassum <30696758+ftabassum@users.noreply.github.com>
2023-05-07 15:47:16 -0400

    code updated with function
    code updated with function

--- distancecalc.py
+++ distancecalc.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb  8 13:34:22 2023
@@ -9,19 +10,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from math import cos, sin, asin, sqrt, radians
+from timedifference import time
 
-## load GTFS MTA Bus Stop file which has stop coordinates (but not according to bus lines) 
-#MTA_Bus_Stops = pd.read_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\stops.txt')
-MTA_Bus_Stops = pd.read_csv('stops.txt')
-MTA_Bus_Stops = MTA_Bus_Stops.drop(["stop_desc", "zone_id", "stop_url", "location_type", "parent_station"], axis=1)
+#%% This code calculates distances between bus stops for a selected bus line. These are the shortest distances between coordinates that do not take into account road geometry.
+# load GTFS MTA Bus Stop file which has stop coordinates (but not according to bus lines) 
 
-## load calculated time difference csv file
-#df_time_M101 = pd.read_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\Time Difference\M101_timedifference.csv')
-df_time_M101 = pd.read_csv('M101_timedifference.csv')
+def speed(line, trip_id_0, trip_id_1, trips, stop_times, stops):
+    # load previously calculated bus line csv files with time difference in both directions
+    #df_ts_bl_0 = pd.read_csv('insert path for bus line 0 direction')
+    #df_ts_bl_1 = pd.read_csv('insert path for bus line 1 direction')
+    
+    MTA_Bus_Stops = pd.read_csv(stops)
+    MTA_Bus_Stops = MTA_Bus_Stops.drop(["stop_desc", "zone_id", "stop_url", "location_type", "parent_station"], axis=1) #these columns are not required in the code
+    
+    df_ts_bl_0, df_ts_bl_1= time(line, trip_id_0, trip_id_1, trips, stop_times)
+    
+    #(run this code as is)
+    # merge time and location dataframes to correspond the time difference to a stop location (coordinates)
+    df_ts_bl_0 = df_ts_bl_0.merge(MTA_Bus_Stops, on = 'stop_id')
+    df_ts_bl_0["distanceinmiles"] = 3960*2 * np.arcsin(np.sqrt(np.sin((np.radians(df_ts_bl_0['stop_lat']) - np.radians(df_ts_bl_0.stop_lat.shift(1)))/2)**2 + np.cos(np.radians(df_ts_bl_0.stop_lon.shift(1))) * np.cos(np.radians(df_ts_bl_0['stop_lon'])) * np.sin((np.radians(df_ts_bl_0['stop_lon']) - np.radians(df_ts_bl_0.stop_lon.shift(1)))/2)**2))
+    df_ts_bl_1 = df_ts_bl_1.merge(MTA_Bus_Stops, on = 'stop_id')
+    df_ts_bl_1["distanceinmiles"] = 3960*2 * np.arcsin(np.sqrt(np.sin((np.radians(df_ts_bl_1['stop_lat']) - np.radians(df_ts_bl_1.stop_lat.shift(1)))/2)**2 + np.cos(np.radians(df_ts_bl_1.stop_lon.shift(1))) * np.cos(np.radians(df_ts_bl_1['stop_lon'])) * np.sin((np.radians(df_ts_bl_1['stop_lon']) - np.radians(df_ts_bl_1.stop_lon.shift(1)))/2)**2))
+    
+    # create a column for cumulative distance
+    df_ts_bl_0["dist_from_start"]=df_ts_bl_0["distanceinmiles"].cumsum(axis=0)
+    df_ts_bl_1["dist_from_start"]=df_ts_bl_1["distanceinmiles"].cumsum(axis=0)
+    #%% This code calculates bus speeds for the M101 for the northbound and southbound routes. 
+    
+    # calculate speeds for each df (run this code as is)
+    df_ts_bl_0["speed"] = df_ts_bl_0["distanceinmiles"]/df_ts_bl_0["timedifference"] 
+    df_ts_bl_1["speed"] = df_ts_bl_1["distanceinmiles"]/df_ts_bl_1["timedifference"]
+    
+    #%% Save each dataframe as a csv file, according to particular bus line name
+    #(insertions required)
+    df_ts_bl_0.to_csv(line+"_northbound_dist.csv", index=False)
+    df_ts_bl_1.to_csv(line+"_southbound_dist.csv", index=False)
 
-# merge time and location dataframes to correspond the time difference to a stop location
-M101 = MTA_Bus_Stops.merge(df_time_M101, on = 'stop_id')
-M101["distanceinmiles"] = 3960*2 * np.arcsin(np.sqrt(np.sin((np.radians(M101['stop_lat']) - np.radians(M101.stop_lat.shift(1)))/2)**2 + np.cos(np.radians(M101.stop_lon.shift(1))) * np.cos(np.radians(M101['stop_lon'])) * np.sin((np.radians(M101['stop_lon']) - np.radians(M101.stop_lon.shift(1)))/2)**2))
+#speed (line, trip_id_northbound, trip_id_southbound, path_to_trips, path_to_time, path_to_bus_stops)
+speed('Q43','QV_B3-Weekday-007000_Q43_551','QV_B3-Weekday-011000_Q43_551', 'trips.txt', 'stop_times.txt', 'stops.txt')
 
-#M101.to_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\Distances\M101 distances.csv')
-M101.to_csv('distances.csv')
--- timedifference.py
+++ timedifference.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Sat Feb  4 20:51:38 2023
@@ -13,34 +14,35 @@ from math import cos, sin, asin, sqrt, radians
 from datetime import timedelta
 
 
-#%% this code loads the GTFS MTA Manhattan trips and shape files. Merging them preserve the sequence of trips
-df_trips = pd.read_csv('trips.txt')
-df_shapes = pd.read_csv('shapes.txt')
-# merging the two files, the first df takes precedence
-df_ts = df_trips.merge(df_shapes, on = "shape_id")
-
-#%%this code loads GTFS MTA Manhattan bus stop times and divides them according to the bus line
-#df_time = pd.read_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\stop_times.txt', sep=',' , header=1, names = ["trip_id", "arrival_time", "departure_time", "stop_id", "stop_sequence", "pickup_type", "drop_off_type", "timepoint"])
-df_time = pd.read_csv('stop_times.txt', sep=',' , header=1, names = ["trip_id", "arrival_time", "departure_time", "stop_id", "stop_sequence", "pickup_type", "drop_off_type", "timepoint"])
-
-
-#adding a new column 'Bus_Line' to only include the bus line name
-df_time['Bus_Line'] = df_time["trip_id"].str.split("_").map(lambda x: x[2])
-
-#masking a dataframe
-df_time_M101 = df_time[["arrival_time", "stop_id", "Bus_Line"]].loc[df_time['Bus_Line']=="M101"]
-df_time_M101.drop_duplicates(subset=["stop_id"], keep="first", inplace=True)
-df_time_M101.head()
-
-# there are a lot of suplicate values in the set, why?
-df_time_M101.drop_duplicates(subset=["stop_id"], keep="first", inplace=True)
-df_time_M101.head()
-
-# this code converts time difference into hours (want speed in mph)
-df_time_M101["timetravelledinhours"] = pd.to_timedelta(df_time_M101["arrival_time"]).dt.total_seconds()/3600
-df_time_M101["timedifference"] = df_time_M101["timetravelledinhours"].diff().abs()
-
-# look at where there are negative times in the dataset and why that is the case
-df_time_M101 = df_time_M101.drop(["timetravelledinhours"], axis=1)
-#df_time_M101.to_csv(r'C:\Users\minah\OneDrive\Desktop\Independent Study\Time Difference\M101_timedifference.csv', index=False)
-df_time_M101.to_csv('M101_timedifference.csv', index=False)
+def time(line, trip_id_0, trip_id_1, trips, stop_times):
+    
+    #%% this code loads the GTFS MTA Manhattan trips and trips files. Merging them preserve the sequence of trips
+    df_trips = pd.read_csv(trips)
+    df_time = pd.read_csv(stop_times)
+    # merging the two files, the first df takes precedence
+    df_ts = df_trips.merge(df_time, on = "trip_id")
+    
+    #%% This is a generalized code that can be applied to any bus line. Comments are made where specific inputs are required
+    # This code masks a dataframe, meaning it reduces the size of the dataframe to contain only the defined value(s) in the defined column(s)
+    df_ts_bl = df_ts[["route_id", "trip_id", "shape_id","trip_headsign", "arrival_time", "stop_id", "stop_sequence", "direction_id"]].loc[df_ts["route_id"]==line] #choose a specific bus line for the route_id
+    
+    #%%                 
+    # This code chooses arbitrary trip_ids to isolate a route running in both directions, defined by a 0 for northbound or eastbound and 1 for southbound or westbound routes. 
+    df_ts_bl_01 = df_ts_bl.loc[(df_ts_bl["trip_id"]==trip_id_0) | (df_ts_bl["trip_id"] ==trip_id_1)]
+    df_ts_bl_0 = df_ts_bl_01.loc[df_ts_bl_01["direction_id"] == 0]
+    df_ts_bl_1 = df_ts_bl_01.loc[df_ts_bl_01["direction_id"] == 1]
+    
+    #%%% Add time difference column to northbound/eastbound and southbound/westbound direction dataframes
+    
+    df_ts_bl_0["timetravelledinhours"] = pd.to_timedelta(df_ts_bl_0["arrival_time"]).dt.total_seconds()/3600 # converting arrival time into seconds and then hours
+    df_ts_bl_0["timedifference"] = df_ts_bl_0["timetravelledinhours"].diff().abs() # takes the difference between index i and i-1, and places it in index i
+    df_ts_bl_0 = df_ts_bl_0.drop(["timetravelledinhours"], axis = 1)
+    df_ts_bl_1["timetravelledinhours"] = pd.to_timedelta(df_ts_bl_1["arrival_time"]).dt.total_seconds()/3600
+    df_ts_bl_1["timedifference"] = df_ts_bl_1["timetravelledinhours"].diff().abs()
+    df_ts_bl_1 = df_ts_bl_1.drop(["timetravelledinhours"], axis = 1)
+    
+    return df_ts_bl_0, df_ts_bl_1 
+    #%%% Save each dataframe as a csv file, according to the particular bus line name
+    
+    #df_ts_bl_0.to_csv(line + '_distance_northbound.csv', index=False) 
+    #df_ts_bl_1.to_csv(line + '_distance_southbound.csv', index=False)
